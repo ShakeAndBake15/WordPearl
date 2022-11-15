58f46fdc4f299b69ee1cff0d83b133b9ed388bb4
Alex Lyon <alexlyon@Alexs-MacBook-Air.local>
2022-11-15 18:02:06 +0000

    hosting troubleshooting

--- wordpearl/settings.py
+++ wordpearl/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9qr0w!*t0=7g(1)w3%g^=x5^wqb25o4lsbxntmkx99@dutn@-f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
