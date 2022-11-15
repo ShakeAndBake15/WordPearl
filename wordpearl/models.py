659625a9707afe28b72e0020df769ae7a0692cf9
wystler <benjamin.rudd@live.com>
2022-11-15 09:24:59 +0000

    add password field

--- wordpearl/models.py
+++ wordpearl/models.py
@@ -2,20 +2,25 @@ from django.db import models
 
 class Oyster(models.Model):
   oyster_id = models.AutoField
+  password = models.CharField(max_length=50)
   username = models.CharField(max_length=50)
   avatar_url = models.CharField(max_length=500)
+  def __str__(self):
+        return self.username
 
 class Pearl(models.Model):
   pearl_id = models.AutoField
   username = models.ForeignKey(Oyster, on_delete=models.CASCADE)
   title = models.CharField(max_length=50)
-  body = models.CharField
+  body = models.CharField(max_length=1000)    ##  added a max-length because it didnt show otherwise
   created_at = models.DateField()
   votes = models.IntegerField()
+  def __str__(self):
+        return self.title
 
 class Comment(models.Model):
   comment_id = models.AutoField
-  body = models.CharField
+  body = models.CharField(max_length=1000)    ##  added a max-length because it didnt show otherwise
   pearl_id = models.ForeignKey(Pearl, on_delete=models.CASCADE)
   username = models.ForeignKey(Oyster, on_delete=models.CASCADE)
   created_at = models.DateField()
