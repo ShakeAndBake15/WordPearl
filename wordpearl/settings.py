ab4b15058ce0c7cbe2f577f509fe9bde8c0d0a0a
Alex Lyon <alexlyon@Alexs-MacBook-Air.local>
2022-11-15 18:46:05 +0000

    hosting troubleshooting

--- wordpearl/settings.py
+++ wordpearl/settings.py
@@ -133,8 +133,8 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
